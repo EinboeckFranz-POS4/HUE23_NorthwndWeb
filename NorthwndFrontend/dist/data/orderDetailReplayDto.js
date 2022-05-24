1a4d627f9b4f0bdac8e10b133e40683d7a4f61b3
EinboeckFranz <EinboeckF180011@sus.htl-grieskirchen.at>
2022-05-24 12:52:38 +0200

    Adjusted style.

--- NorthwndFrontend/dist/data/orderDetailReplayDto.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoib3JkZXJEZXRhaWxSZXBsYXlEdG8uanMiLCJzb3VyY2VSb290IjoiLi9zcmMvIiwic291cmNlcyI6WyJkYXRhL29yZGVyRGV0YWlsUmVwbGF5RHRvLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
